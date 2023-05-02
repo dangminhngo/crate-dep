95db482388b9faa05c9134fedc0a8dff2babbbbf
dangminhngo <dangminhngo.dev@gmail.com>
2023-05-03 00:36:48 +0700

    fix(editor): add titlebar

--- apps/client/src/pages/app/note.tsx
+++ apps/client/src/pages/app/note.tsx
@@ -1,29 +1,45 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Flex, Text } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 
 import Editor from '@/components/editor'
 import { useNoteById } from '@/hooks'
+import { formatDateTime } from '@/lib/helpers'
 
 export default function NotePage() {
   const params = useParams()
-  const { status, data } = useNoteById(params.id as string)
+  const { status, data: note } = useNoteById(params.id as string)
 
   if (status === 'loading') return <div>Loading...</div>
   if (status === 'error') return <div>Loading...</div>
 
   return (
     <Container flex={1}>
-      <Editor
-        code={data.code}
-        setCode={(c) => console.log(c)}
-        config={{
-          autocomplete: true,
-          highlightActiveLine: true,
-          lineNumbers: true,
-          lineWrapping: true,
-          tabSize: 2,
-        }}
-      />
+      <Flex height="100%" direction="column" align="stretch">
+        <Flex
+          align="center"
+          justify="space-between"
+          bg="slate.950"
+          px={6}
+          py={3}
+        >
+          <Text fontWeight="medium">{note.title}</Text>
+          <Text color="slate.400">
+            Edited at {formatDateTime(note.updatedAt, 'HH:MM')} on{' '}
+            {formatDateTime(note.updatedAt, 'LLL dd, yyyy')}
+          </Text>
+        </Flex>
+        <Editor
+          code={note.code}
+          setCode={(c) => console.log(c)}
+          config={{
+            autocomplete: true,
+            highlightActiveLine: true,
+            lineNumbers: true,
+            lineWrapping: true,
+            tabSize: 2,
+          }}
+        />
+      </Flex>
     </Container>
   )
 }

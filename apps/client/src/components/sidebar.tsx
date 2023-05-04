99d6fa2b91e816e1b767815957a1bb7f6766b0c2
dangminhngo <dangminhngo.dev@gmail.com>
2023-05-05 00:07:17 +0700

    feat: add profile button with dropdown menu

--- apps/client/src/components/sidebar.tsx
+++ apps/client/src/components/sidebar.tsx
@@ -1,15 +1,71 @@
-import { Search, Settings, Star, StickyNote, Tag, Trash } from 'lucide-react'
+import { useAuth0 } from '@auth0/auth0-react'
+import {
+  ChevronDown,
+  LogOut,
+  Search,
+  Settings,
+  Star,
+  StickyNote,
+  Tag,
+  Trash,
+  User,
+} from 'lucide-react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-import { SectionButton } from './ui'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  Icon,
+  SectionButton,
+} from './ui'
 
 export default function Sidebar() {
+  const { user, logout } = useAuth0()
   const navigate = useNavigate()
   const { pathname } = useLocation()
 
   return (
-    <div className="flex min-w-[256px] flex-col items-stretch justify-between bg-slate-950 p-4">
+    <div className="flex min-w-[272px] flex-col items-stretch justify-between bg-slate-950 p-4">
       <div className="flex flex-col items-stretch gap-6">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              className="h-10 justify-between focus:outline-none"
+            >
+              <div className="flex items-center gap-4">
+                <Avatar className="h-[18px] w-[18px]">
+                  <AvatarImage src={user?.picture} alt={user?.name} />
+                  <AvatarFallback>{user?.name?.charAt(0)}</AvatarFallback>
+                </Avatar>
+                <span>{user?.name}</span>
+              </div>
+              <Icon as={ChevronDown} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuContent className="min-w-[240px]">
+              <DropdownMenuItem className="flex items-center gap-4">
+                <Icon as={User} />
+                <span>Account</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-4 text-red-400"
+                onClick={() => logout()}
+              >
+                <Icon as={LogOut} />
+                <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenuPortal>
+        </DropdownMenu>
         <div className="flex flex-col items-stretch gap-1">
           <SectionButton
             icon={Search}
--- apps/client/src/components/ui/index.ts
+++ apps/client/src/components/ui/index.ts
@@ -1,5 +1,7 @@
+export * from './avatar'
 export * from './button'
 export * from './chip'
+export * from './dropdown-menu'
 export * from './icon-button'
 export * from './icon'
 export * from './section-button'

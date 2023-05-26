ec42fb34d4d262e8bf0309c457396a2494e547e4
dangminhngo <dangminhngo.dev@gmail.com>
2023-05-26 11:44:46 +0700

    fix: use `tests` folder instead

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ const opts = {
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */

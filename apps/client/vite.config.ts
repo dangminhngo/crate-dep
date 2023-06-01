01cd50ef511b7ff10b3d4e5db88b51499c2e723e
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-02 04:52:19 +0700

    fix: change script 'start' to 'preview'

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react(), splitVendorChunkPlugin()],
+  build: {},
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },

6086b5cb7dc680646ac6dbd88f92be93f5097aa6
dangminhngo <dangminhngo.dev@gmail.com>
2023-07-11 14:23:13 +0700

    fix(client): change alias src to `~`, remove server option in vite config

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,16 +5,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
-  build: {},
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 3000,
+    alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
   },
 })

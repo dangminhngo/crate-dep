d9b4f919607ec3705605de558911e484bbc65adc
dangminhngo <dangminhngo.dev@gmail.com>
2023-07-12 08:07:44 +0700

    fix: fix eslint packages

--- packages/eslint-config-custom-server/index.js
+++ packages/eslint-config-custom-server/index.js
@@ -1,21 +1,18 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2020: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'turbo',
     'plugin:prettier/recommended',
   ],
-  plugins: ['@typescript-eslint', 'react-refresh'],
+  plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   rules: {
-    'react-refresh/only-export-components': 'warn',
-    'react/jsx-key': 'off',
     // @link https://stackoverflow.com/questions/76457373/cannot-read-properties-of-undefined-reading-gettokens-occurred-while-linting
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
